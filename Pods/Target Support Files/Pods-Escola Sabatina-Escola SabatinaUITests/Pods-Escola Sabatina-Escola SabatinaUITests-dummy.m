c6da495127d94bd30872fb37a6ccc2f7468ac23d
masblima <masblima@rd.com.br>
2023-06-30 10:22:07 -0300

    remove pods

--- Pods/Target Support Files/Pods-Escola Sabatina-Escola SabatinaUITests/Pods-Escola Sabatina-Escola SabatinaUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Escola_Sabatina_Escola_SabatinaUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Escola_Sabatina_Escola_SabatinaUITests
-@end
